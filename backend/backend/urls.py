d3262cd0c83d40cd9dedae95fdf0234372388e9e
Maxim Nikolaev <maxoutpost@outlook.com>
2025-09-01 22:38:20 +0300

    workflow with PEP8 linting

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,9 @@
 """Эндпоинты для проекта."""
 
+from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-
-from api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
